3164bbb3310e7e4eaa45def052543f1d522962a3
komeno <tkomeno@student.42tokyo.jp>
2022-05-02 05:25:32 -0300

    ✨ check

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # Check Review Go Piscine (42 Tokyo)
 
-## USAGE:
+## Usage:
 First source the file to be able to `cd` into other directories:
 ```sh
 source check.sh
 
 ```
 
-## EXPLANATION:
+## Explanation:
 You should execute the command like this:
 ```sh
 ./check.sh ACTION (e.g. see/edit/run) LESSON (e.g. Go00) EXERCISE (e.g. ex00)
 
 ```
 
-## USE EXAMPLE:
+## Use Example:
 Use `./check.sh see 00 01` (to see Go00's ex01 contents using cat/bat)
 Use `./check.sh edit 00 01` (to edit Go00's ex01 using vim)
 Use `./check.sh run 00 01` (to run Go00's ex01)
--- check.sh
+++ check.sh
@@ -6,24 +6,11 @@
 #    By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/29 06:36:24 by tkomeno           #+#    #+#              #
-#    Updated: 2022/04/29 09:43:38 by tkomeno          ###   ########.fr        #
+#    Updated: 2022/05/02 05:25:28 by tkomeno          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
-
-# USAGE:
-# First source the file to be able to cd into other directories:
-# "source check.sh"
-
-# EXPLANATION:
-# You should execute the command like this:
-# ./check.sh ACTION (e.g. see/edit/run) LESSON (e.g. Go00) EXERCISE (e.g. ex00)
-
-# USE EXAMPLE:
-# Use "./check.sh see 00 01" (to see Go00's ex01 contents using cat/bat)
-# Use "./check.sh edit 00 01" (to edit Go00's ex01 using vim)
-# Use "./check.sh run 00 01" (to run Go00's ex01)
 
 if [[ $1 == "see" ]]; then
 	$(! command -v bat &> /dev/null && echo "cat" || echo "bat") \
